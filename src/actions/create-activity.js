7ad0d8e8cb5da5f62a8f874a468e88437f126500
Qlaramedbindestreg <u2470787@rts-365.dk>
2025-09-03 08:40:23 +0200

    add create-activity and delete-activity

---
+++ src/actions/create-activity.js
@@ -0,0 +1,10 @@
+"use server";
+
+import { cookies } from "next/headers";
+import {revalidatePath} from "next/cache";
+
+//Kilde: Repitition: /Users/qlara/Desktop/coding/next/next-repetition/src/actions/create-kageperson.js
+
+export default async function createActivity(prevState, formData) {
+    return ( null )
+}
---
+++ src/actions/delete-activity.js
@@ -0,0 +1,8 @@
+import { cookies } from "next/headers";
+import { revalidatePath } from "next/cache";
+
+// Kilde: Repitition: /Users/qlara/Desktop/coding/next/next-repetition/src/actions/delete-kage.js
+
+export default async function deleteActivity(prevState, formData) {
+    return ( null )
+}
